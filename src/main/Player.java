347db41d0be7bcc353af266347457bef0c24d755
joacotol <joacochrist@gmail.com>
2022-02-25 15:25:06 -0500

    first commit

---
+++ src/main/Player.java
@@ -0,0 +1,44 @@
+package main;
+
+public class Player {
+
+    private String name;
+    private double health;
+    private double attack;
+
+    public Player() {
+        this("John Doe", 10.0, 2.0);
+    }
+
+    public Player(String name, double health, double attack) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public void setAttack(double attack) {
+        this.attack = attack;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public double getHealth() {
+        return this.health;
+    }
+
+    public double getAttack() {
+        return this.attack;
+    }
+}
